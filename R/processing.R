c5d230a01974679e4399043d20d2cc2fe3793db8
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-12-27 15:22:38 +1100

    simplified charts

--- R/processing.R
+++ R/processing.R
@@ -4,6 +4,7 @@ countByYear <- function(data){
     summarise(count = n())
   
   data$rolling_3 <- round(zoo::rollmean(data$count, 3, fill = "extend"), 2)
+  data$rolling_5 <- round(zoo::rollmean(data$count, 5, fill = "extend"), 2)
   data$rolling_10 <- round(zoo::rollmean(data$count, 10, fill = "extend"), 2)
   
   return(data)
@@ -26,4 +27,19 @@ mostPopularRemakes <- function(data, movieMetaData){
     mutate(imdbrating = as.numeric(imdbrating)) %>%
     filter(!is.na(imdbrating)) %>%
     arrange(desc(imdbrating))
+}
+
+medianBoxOfficebyYear <- function(remakes, movieMetaData){
+  medianBoxOfficebyYear <- remakes %>% left_join(movieMetaData, by = c("remakes_key" = "key")) %>%
+    select(year.x, boxOffice, imdbrating) %>%
+    filter(!is.na(year.x), boxOffice != "N/A") %>%
+    mutate(boxOffice = str_replace_all(boxOffice, ",", ""),
+           boxOffice = str_replace_all(boxOffice, "\\$", "")) %>%
+    group_by(year.x) %>%
+    summarise(boxOffice = median(as.numeric(boxOffice)),
+              averageRating = mean(as.numeric(imdbrating)),
+              totalMovies = n()) %>%
+    rename("year" = year.x)
+  
+  return(medianBoxOfficebyYear)
 }
--- R/visualisations.R
+++ R/visualisations.R
@@ -1,9 +1,11 @@
-countByYear_chart <- function(data, colours){
+countByYear_chart <- function(data, colours, title){
   highchart() %>%
+    hc_title(text = title, align = "left", margin = 20) %>%
     hc_xAxis(categories = unique(data$year)) %>%
     hc_add_series(data = data$count, type = "column", color = colours[1], name = "Count of Movies") %>% 
-    hc_add_series(data = data$rolling_3, type = "line", color = colours[2], name = "3 Year Rolling Average") %>% 
-    hc_add_series(data = data$rolling_10, type = "line", color = colours[3], name = "10 Year Rolling Average")
+    # hc_add_series(data = data$rolling_3, type = "line", color = colours[2], name = "3 Year Rolling Average") %>% 
+    hc_add_series(data = data$rolling_5, type = "line", color = colours[3], name = "5 Year Rolling Average")
+    # hc_add_series(data = data$rolling_10, type = "line", color = colours[3], name = "10 Year Rolling Average")
 }
 
 tableWithBar <- function(data, colNames, barColour, range, pageLength = 30){
@@ -14,4 +16,12 @@ tableWithBar <- function(data, colNames, barColour, range, pageLength = 30){
                                                                         backgroundSize = '98% 88%',
                                                                         backgroundRepeat = 'no-repeat',
                                                                         backgroundPosition = 'right')
+}
+
+bubble_chart <- function(data, colours){
+  hchart(data, hcaes(year, boxOffice, size = totalMovies, color = colours[1]), type = "bubble")
+  
+  highchart() %>%
+    hc_xAxis(categories = unique(data$year)) %>%
+    hc_add_series(data = data$boxOffice, size = data$totalMovies, type = "bubble", color = colours[1], name = "Count of Movies")
 }
