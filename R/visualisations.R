6242172ae083bfc79ef0fb118d0570eabab5509d
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-12-31 14:41:08 +1100

    refactor

--- R/visualisations.R
+++ R/visualisations.R
@@ -1,11 +1,24 @@
-countByYear_chart <- function(data, colours, title){
+countByYear_chart <- function(data, colours, title, subtitle, source, link){
   highchart() %>%
-    hc_title(text = title, align = "left", margin = 20) %>%
+    hc_title(text = paste0("<b>", title, "</b>"), align = "left", margin = 20) %>%
+    hc_subtitle(text = subtitle, align = "left") %>%
+    hc_credits(enabled = TRUE, text = source, href = link, position = list(align = "left", x = 12)) %>%
     hc_xAxis(categories = unique(data$year)) %>%
     hc_add_series(data = data$count, type = "column", color = colours[1], name = "Count of Movies") %>% 
     # hc_add_series(data = data$rolling_3, type = "line", color = colours[2], name = "3 Year Rolling Average") %>% 
     hc_add_series(data = data$rolling_5, type = "line", color = colours[3], name = "5 Year Rolling Average")
     # hc_add_series(data = data$rolling_10, type = "line", color = colours[3], name = "10 Year Rolling Average")
+}
+
+popularRemakes_chart <- function(data, colours, title, subtitle, source, link){
+  hchart(data, hcaes(remakes_key, as.numeric(imdbrating), color = colours, label = remakes_year), type = "bar", height = "200px") %>% 
+  hc_plotOptions(bar = list(stacking = "null")) %>% 
+  hc_xAxis(title = list(enabled = FALSE)) %>% 
+  hc_yAxis(title = list(enabled = FALSE)) %>%
+  hc_chart(height = "200") %>%
+  hc_title(text = paste0("<b>", title, "</b>"), align = "left", margin = 20) %>%
+  hc_subtitle(text = subtitle, align = "left") %>%
+  hc_credits(enabled = TRUE, text = source, href = link, position = list(align = "left", x = 12))
 }
 
 tableWithBar <- function(data, colNames, barColour, range, pageLength = 30){
@@ -25,3 +38,11 @@ bubble_chart <- function(data, colours){
     hc_xAxis(categories = unique(data$year)) %>%
     hc_add_series(data = data$boxOffice, size = data$totalMovies, type = "bubble", color = colours[1], name = "Count of Movies")
 }
+
+barChart <- function(data){
+  hchart(data, hcaes(primaryGenre, as.numeric(percentage), color = colours[1]), type = "bar")
+}
+
+# df <- data.frame(key = c('phone', 'income'), value = c(10, 90))
+# 
+# hchart(df, hcaes(key, value, color = colours), type = "treemap")
---
+++ scripts/tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE movies (
+  tconst varchar,
+  title_type varchar,
+  primary_title varchar,
+  original_title varchar,
+  is_adult varchar,
+  start_year varchar,
+  end_year varchar,
+  runtime_mins varchar,
+  genres varchar);
+
+CREATE TABLE ratings (
+tconst varchar,
+average_rating int,
+num_votes int);
